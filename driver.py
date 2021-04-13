44da583fcb4f9db296da4907a3acaceb95763853
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-04-13 00:26:05 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,7 +4,7 @@
 from selenium import webdriver
 import keys as key
 
-driver = webdriver.Firefox(executable_path=r'C:\Users\Ryan\Documents\GitHub\investment-interface\geckodriver.exe')
+driver = webdriver.Firefox(executable_path=key.driverpath)
 driver.get("https://investor.vanguard.com/my-account/log-on")
 user_input = driver.find_element_by_id('username')
 user_input.send_keys(key.username)
@@ -15,12 +15,14 @@ login = driver.find_element_by_css_selector('.vui-button')
 login.click()
 
 driver.get('https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/CostBasisSummary.xhtml')
+
+
 #cost_basis = driver.find_element_by_id('cbaPortfolioSummary:costBasisTabs_tabBoxItemLink0')
 #cost_basis.click()
 
 #cost_basis = driver.find_element_by_id('unrealizedTabForm:dataTableAccount_34169838_HoldingAccount_128tbody0')
 
-driver.find_element_by_xpath('/html/body/div[3]/div[6]/div[1]/span/form/div[1]/span[2]/div/div/div[2]/div[1]/span/div[1]/span/div/div[1]/span/span[1]/table/tbody/tr[11]')
+#driver.find_element_by_xpath('/html/body/div[3]/div[6]/div[1]/span/form/div[1]/span[2]/div/div/div[2]/div[1]/span/div[1]/span/div/div[1]/span/span[1]/table/tbody/tr[11]')
 
 print(cost_basis.text)
 
